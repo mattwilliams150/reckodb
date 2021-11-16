ca5cc31a26b410e2eacfa9f8760328246a031841
mattwilliams150 <matthewdavidwilliams@me.com>
2021-11-16 07:26:57 +0000

    remove hardcoded tags

--- index.js
+++ index.js
@@ -30,13 +30,15 @@ mongodb.MongoClient.connect(url, {
         for (var i = 0; i < source.length; i++) {
 
 
-            // change tag names to tagids then populate tag flags. set all to zero first.
-            for (tag in categories.tagIds) {
-                source[i][categories.tagIds[tag]] = 0;
+            // set tag to 1 if in default tags, otherwise set to zero.
+            var tags = {};
+            for (tag in categories.tagObj) {
+                if (categories.tagObj[tag].name == source[i]["tag1"] || categories.tagObj[tag].name == source[i]["tag2"] || categories.tagObj[tag].name == source[i]["tag3"]) {
+                    tags[tag] = 1;
+                } else {
+                    tags[tag] = 0;
+                }
             }
-            source[i][categories.tagIds[source[i]["tag1"]]] = 1;
-            source[i][categories.tagIds[source[i]["tag2"]]] = 1;
-            source[i][categories.tagIds[source[i]["tag3"]]] = 1;
 
             if (source[i]["sw4"] == 1) {
                 var location = "Clapham"
@@ -70,41 +72,7 @@ mongodb.MongoClient.connect(url, {
                 tag3: source[i]["tag3"],
                 subcategory: source[i]["subcategory"],
                 amenities: source[i]["amenities"],
-                culturally_authentic_dishes: source[i]["culturally_authentic_dishes"],
-                exciting_menu: source[i]["exciting_menu"],
-                fresh_flavours: source[i]["fresh_flavours"],
-                generous_portions: source[i]["generous_portions"],
-                fine_dining: source[i]["fine_dining"],
-                small_plates_sharing: source[i]["small_plates_sharing"],
-                street_food_vibes: source[i]["street_food_vibes"],
-                delicious_cocktails: source[i]["delicious_cocktails"],
-                craft_local_beer: source[i]["craft_local_beer"],
-                fantastic_wine_list: source[i]["fantastic_wine_list"],
-                awesome_architecture: source[i]["awesome_architecture"],
-                entertaining: source[i]["entertaining"],
-                fantastic_service: source[i]["fantastic_service"],
-                luxurious: source[i]["luxurious"],
-                buzzing_atmosphere: source[i]["buzzing_atmosphere"],
-                great_music: source[i]["great_music"],
-                intimate: source[i]["intimate"],
-                modern_design: source[i]["modern_design"],
-                party_vibes: source[i]["party_vibes"],
-                quirky: source[i]["quirky"],
-                relaxing: source[i]["relaxing"],
-                romantic: source[i]["romantic"],
-                rustic: source[i]["rustic"],
-                chic: source[i]["chic"],
-                working_from_home: source[i]["working_from_home"],
-                young_kids_families: source[i]["young_kids_families"],
-                large_groups: source[i]["large_groups"],
-                vegans_vegetarians: source[i]["vegans_vegetarians"],
-                bringing_the_dog: source[i]["bringing_the_dog"],
-                a_date: source[i]["a_date"],
-                a_special_occasion: source[i]["a_special_occasion"],
-                a_quiet_catch_up: source[i]["a_quiet_catch_up"],
-                watching_sport: source[i]["watching_sport"],
-                an_evening_with_friends: source[i]["an_evening_with_friends"],
-                bottomless_brunch: source[i]["bottomless_brunch"]
+                tags: tags
              };
              arrayToInsert.push(oneRow);
          }

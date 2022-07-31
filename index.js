cceae47848ee95063ae42117a25c1f8faf01042f
Zin Eddine Lamziouik <zineddinelamziouik@gmail.com>
2022-07-31 22:58:28 +0100

    fixed the tags issue and switching to mongoose

--- index.js
+++ index.js
@@ -1,100 +1,94 @@
-// Import required module csvtojson and mongodb packages
+// Import required module csvtojson, mongoose and dotenv packages
 const csvtojson = require('csvtojson');
-const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+require("dotenv").config()
 const categories = require('../recko/config/categories.json')
-const db_strings = require("./db_strings.json");
+const Places = require('./models/places');
 
-// choose env [local,qa,prod]
-var url = db_strings.local;
-console.log(url);
-
-var dbConn;
-mongodb.MongoClient.connect(url, {
-    useUnifiedTopology: true,
-}).then((client) => {
-    console.log('DB Connected!');
-    dbConn = client.db();
-    
-    // CSV file name
-    const fileName = "places.csv";
-    var arrayToInsert = [];
-
-    //inserting into the table "places"
-    var collectionName = 'places';
-    var collection = dbConn.collection(collectionName);
+//connect to the database using mongoose
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(async (res) => {
+		console.log('DB Connected!');
+		
+		// CSV file name
+		const fileName = "places.csv";
+		var arrayToInsert = [];
 
 
-    csvtojson().fromFile(fileName).then(source => {
-        // Fetching the all data from each row
+		csvtojson().fromFile(fileName).then(source => {
+			// Fetching the all data from each row
+			let t = 1;
+			for (var i = 0; i < source.length; i++) {
 
-        for (var i = 0; i < source.length; i++) {
 
+				// set tag to 1 if in default tags, otherwise set to zero.
+				let tags = {};
+				for (let tag in categories.tagObj) {
+					if (categories.tagObj[tag].name == source[i]["tag1"] || categories.tagObj[tag].name == source[i]["tag2"] || categories.tagObj[tag].name == source[i]["tag3"]) {
+						tags[tag] = 1;
+					} else {
+						tags[tag] = 0;
+					}
+				}
+				tags["number"] = t;
+				t++;
+				console.log(tags);
 
-            // set tag to 1 if in default tags, otherwise set to zero.
-            var tags = {};
-            for (tag in categories.tagObj) {
-                if (categories.tagObj[tag].name == source[i]["tag1"] || categories.tagObj[tag].name == source[i]["tag2"] || categories.tagObj[tag].name == source[i]["tag3"]) {
-                    tags[tag] = 1;
-                } else {
-                    tags[tag] = 0;
-                }
-            }
+				// if (source[i]["sw4"] == 1) {
+				// 	var location = "Clapham"
+				// } else if (source[i]["sw11"] == 1) {
+				// 	var location = "Clapham Junction & Battersea"
+				// } else if (source[i]["sw12"] == 1) {
+				// 	var location = "Clapham South & Balham"
+				// } else {
+				// 	var location = "unknown"
+				// }
 
-            if (source[i]["sw4"] == 1) {
-                var location = "Clapham"
-            } else if (source[i]["sw11"] == 1) {
-                var location = "Clapham Junction & Battersea"
-            } else if (source[i]["sw12"] == 1) {
-                var location = "Clapham South & Balham"
-            } else {
-                var location = "unknown"
-            }
+				// var oneRow = {
+				// 	placeId: source[i]["placeId"],
+				// 	photo: source[i]["photo"],
+				// 	lat: source[i]["lat"],
+				// 	long: source[i]["long"],
+				// 	placeName: source[i]["placeName"],
+				// 	review: source[i]["review"],
+				// 	price: source[i]["price"],
+				// 	address: source[i]["address"],
+				// 	location: location,
+				// 	sw4: source[i]["sw4"],
+				// 	sw11: source[i]["sw11"],
+				// 	sw12: source[i]["sw12"],
+				// 	telephone: source[i]["telephone"],
+				// 	website: source[i]["website"],
+				// 	description: source[i]["description"],
+				// 	type: source[i]["type"],
+				// 	tag1: source[i]["tag1"],
+				// 	tag2: source[i]["tag2"],
+				// 	tag3: source[i]["tag3"],
+				// 	subcategory: source[i]["subcategory"],
+				// 	amenities: source[i]["amenities"],
+				// 	tags: tags
+				// };
+				// console.log(oneRow);
+			}
+		});
+		// 	if(collection) {
+		// 		collection.drop(function(err, delOK) {
+		// 			if (err) throw err;
+		// 			if (delOK) console.log("Collection deleted");
+		// 		});
+		// 	}
 
-             var oneRow = {
-                placeId: source[i]["placeId"],
-                photo: source[i]["photo"],
-                lat: source[i]["lat"],
-                long: source[i]["long"],
-                placeName: source[i]["placeName"],
-                review: source[i]["review"],
-                price: source[i]["price"],
-                address: source[i]["address"],
-                location: location,
-                sw4: source[i]["sw4"],
-                sw11: source[i]["sw11"],
-                sw12: source[i]["sw12"],
-                telephone: source[i]["telephone"],
-                website: source[i]["website"],
-                description: source[i]["description"],
-                type: source[i]["type"],
-                tag1: source[i]["tag1"],
-                tag2: source[i]["tag2"],
-                tag3: source[i]["tag3"],
-                subcategory: source[i]["subcategory"],
-                amenities: source[i]["amenities"],
-                tags: tags
-             };
-             arrayToInsert.push(oneRow);
-         }
-
-        if(collection) {
-            collection.drop(function(err, delOK) {
-                if (err) throw err;
-                if (delOK) console.log("Collection deleted");
-            });
-        }
-
-         collection.insertMany(arrayToInsert, (err, result) => {
-             if (err) console.log(err);
-             if(result){
-                 console.log("Import CSV into database successfully.");
-             }
-         });
-    });
-    
-    
-}).catch(err => {
-    console.log("DB Connection Error: ${err.message}");
-});
+		// 	collection.insertMany(arrayToInsert, (err, result) => {
+		// 		if (err) console.log(err);
+		// 		if(result){
+		// 			console.log("Import CSV into database successfully.");
+		// 		}
+		// 	});
+		// });
+		
+		
+	}).catch(err => {
+		console.log("DB Connection Error: ${err.message}");
+	});
 
 
---
+++ models/places.js
@@ -0,0 +1,30 @@
+
+var mongoose = require('mongoose');
+
+var placesSchema = mongoose.Schema({
+    placeId: {type: String, required: true},
+    photo: {type: String},
+    lat: {type: String},
+    long: {type: String},
+    placeName: {type: String},
+    review: {type: String},
+    price: {type: String},
+    address: {type: String},
+    location: {type: String},
+    sw4: {type: String},
+    sw11: {type: String},
+    sw12: {type: String},
+    telephone: {type: String},
+    website: {type: String},
+    description: {type: String},
+    type: {type: String},
+    tag1: {type: String},
+    tag2: {type: String},
+    tag3: {type: String},
+    subcategory: {type: String},
+    amenities: {type: String},
+    tags: {type: Object},
+    posTags: {type: Object}
+});
+
+module.exports = mongoose.model('Places', placesSchema);
